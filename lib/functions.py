e29a8882f90fe1a3f7d79b0afa040b7b3f7e4818
atielijohn8 <atielijohn4@gmail.com>
2025-08-04 23:45:33 +0300

    js/py.conversion

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+greet_programmer()
 
-def greet(name):
-    pass
+def greet(name = "Naureen" ):
+    print(f"Hello, {name}!")
+greet()
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default()
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    #print(num1  + num2)
+    return num1 + num2
+add(1, 2)
 
 def halve(number):
-    pass
+    if type(number) not in (int, float):
+        return None
+    return number / 2
